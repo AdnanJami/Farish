6e8699edb23203bf6d7bb128a7f706097404d2c1
AdnanJami <muntasiradnanj@gmail.com>
2026-05-18 20:05:16 +0600

    Fix debug

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG=False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,farish-hrx1.onrender.com').split(',')
 
